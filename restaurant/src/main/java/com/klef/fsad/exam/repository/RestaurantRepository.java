389a921d39126f1dc333ce6692fb5560d64ce025
2400032670klu <2400032670@kluniversity.in>
2026-03-13 10:01:49 +0530

    Delete restaurant directory

--- restaurant/src/main/java/com/klef/fsad/exam/repository/RestaurantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.klef.fsad.exam.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.klef.fsad.exam.entity.Restaurant;
-
-public interface RestaurantRepository extends JpaRepository<Restaurant,Integer>{
-
-}
